9a5f715788f83be01c507175e40f6592cc6e3267
5h5ong <Call5h5ong@gmail.com>
2019-10-02 22:26:59 +0900

    make obejct url

--- src/Components/Streaming/Streaming.tsx
+++ src/Components/Streaming/Streaming.tsx
@@ -2,16 +2,27 @@ import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 
 const Streaming = () => {
-  const [video, setVideo] = useState();
   const downloadVideo = async () => {
-    const video = await axios.post('http://localhost:4000/video', {
-      videoName: 'sample-video.mp4'
-    });
-    setVideo(video);
+    const video = await axios.post(
+      'http://localhost:4000/video',
+      {
+        videoName: 'sample-video.mp4'
+      },
+      {
+        responseType: 'blob'
+      }
+    );
+    // make object url
+    const videoUrl = URL.createObjectURL(video.data);
+    return videoUrl;
+  };
+  const makeBlob = (videoData: any) => {
+    const blob = new Blob(videoData, { type: 'video/mp4' });
+    return blob;
   };
   useEffect(() => {
     downloadVideo();
-  }, []);
+  });
 
   return <div>hello!</div>;
 };
