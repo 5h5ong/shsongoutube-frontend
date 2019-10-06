20ebae5128d66d5a5a52340e6d810477e1def535
5h5ong <Call5h5ong@gmail.com>
2019-10-06 21:43:16 +0900

    Apollo preferences

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { ApolloProvider } from 'react-apollo-hooks';
+import Client from './apolloClient';
 import App from './App';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+  <ApolloProvider client={Client}>
+    <App />
+  </ApolloProvider>,
+  document.getElementById('root')
+);
