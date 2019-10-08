8c2b80af3fc669f3dc16ad8a8710f83df687d345
5h5ong <Call5h5ong@gmail.com>
2019-10-08 22:59:39 +0900

    add button color

--- src/Styles/Style.ts
+++ src/Styles/Style.ts
@@ -1,6 +1,7 @@
 export const theme = {
   boxShadow:
     '0 12px 19px 2px rgba(34,0,51,.04),0 6px 22px 4px rgba(7,48,114,.12),0 6px 10px -4px rgba(14,13,26,.12);',
-  bgColor: '#fbfbfe'
+  bgColor: '#fbfbfe',
+  btnColor: '#0060df'
 };
 export type Theme = typeof theme;
