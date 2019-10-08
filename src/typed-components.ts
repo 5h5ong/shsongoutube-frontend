a41acffb623cc6d68c3e90389964b430e37a4b0e
5h5ong <Call5h5ong@gmail.com>
2019-10-08 22:49:49 +0900

    필요없는 부분 제거

--- src/typed-components.ts
+++ src/typed-components.ts
@@ -3,6 +3,7 @@ import { ThemedStyledComponentsModule } from 'styled-components';
 import { Theme } from './Styles/Style';
 
 // Theme에 기반한 styled-components의 모듈들?
+// theme의 Type을 typescript가 알게 만들어줌.
 const {
   default: styled,
   createGlobalStyle,
