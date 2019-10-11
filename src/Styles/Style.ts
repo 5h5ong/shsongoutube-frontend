662157bdace3e6d4e709ad4ccc52b6b381defed7
5h5ong <Call5h5ong@gmail.com>
2019-10-11 15:15:27 +0900

    add whiteBox style

--- src/Styles/Style.ts
+++ src/Styles/Style.ts
@@ -1,7 +1,15 @@
+const BOX_SHADOW =
+  '0 12px 19px 2px rgba(34,0,51,.04),0 6px 22px 4px rgba(7,48,114,.12),0 6px 10px -4px rgba(14,13,26,.12);';
+const BORDER_RADIUS = '16px';
+
 export const theme = {
-  boxShadow:
-    '0 12px 19px 2px rgba(34,0,51,.04),0 6px 22px 4px rgba(7,48,114,.12),0 6px 10px -4px rgba(14,13,26,.12);',
   bgColor: '#fbfbfe',
-  btnColor: '#0060df'
+  btnColor: '#0060df',
+  whiteBox: `background-color: white;
+             box-shadow: ${BOX_SHADOW};
+             padding: 30px;
+             border-radius: ${BORDER_RADIUS};
+             justify-content: center;
+  `
 };
 export type Theme = typeof theme;
