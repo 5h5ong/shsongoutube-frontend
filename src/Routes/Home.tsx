777926923ac05af941e59c597076d5408f0df27c
5h5ong <Call5h5ong@gmail.com>
2019-10-13 18:26:31 +0900

    make getAllFile query

--- src/Routes/Home.tsx
+++ src/Routes/Home.tsx
@@ -1,7 +1,26 @@
 import React from 'react';
+import { useQuery } from 'react-apollo-hooks';
+import { gql } from 'apollo-boost';
+
+const GET_ALL_FILE = gql`
+  {
+    getAllFile {
+      id
+      url
+      filename
+    }
+  }
+`;
 
 const Home = () => {
-  return <div>Home</div>;
+  const { data, loading } = useQuery(GET_ALL_FILE);
+  console.log(data);
+  return (
+    <>
+      {loading && <div>Loading...</div>}
+      {!loading && data && data.getAllFile && <div>Beautiful!</div>}
+    </>
+  );
 };
 
 export default Home;
