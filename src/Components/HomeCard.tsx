1715077ddff64df37823b47844437ed1e795cfdd
5h5ong <Call5h5ong@gmail.com>
2019-10-13 18:55:36 +0900

    기본 구조 완성

--- src/Components/HomeCard.tsx
+++ src/Components/HomeCard.tsx
@@ -7,6 +7,21 @@ type HomeCardProps = {
   url: string;
 };
 
-const HomeCard = ({ id, filename, url }: HomeCardProps) => {};
+const Container = styled.div`
+  ${props => props.theme.whiteBox}
+  margin: 0 20px;
+`;
+
+const BoldText = styled.div`
+  font-size: 20px;
+`;
+
+const HomeCard = ({ id, filename, url }: HomeCardProps) => {
+  return (
+    <Container>
+      <BoldText>{filename}</BoldText>
+    </Container>
+  );
+};
 
 export default HomeCard;
