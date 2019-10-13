f420c2c25e855646de2d311f85141d0d63583c74
5h5ong <Call5h5ong@gmail.com>
2019-10-13 20:04:35 +0900

    change videoId type

--- src/Routes/Detail.tsx
+++ src/Routes/Detail.tsx
@@ -1,7 +1,16 @@
 import React from 'react';
+import { withRouter } from 'react-router-dom';
+import VideoPlayer from '../Components/VideoPlayer';
 
-const Detail = () => {
-  return <div>Detail</div>;
+const Detail = ({ match, location }: any) => {
+  const { filename } = match.params;
+  const { videoId } = location.state;
+  return (
+    <>
+      <div>{filename}</div>
+      <VideoPlayer id={parseInt(videoId)} />
+    </>
+  );
 };
 
-export default Detail;
+export default withRouter(Detail);
