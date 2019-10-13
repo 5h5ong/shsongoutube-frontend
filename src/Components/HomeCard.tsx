13beed35d65088a3b3b91c298caca3adb007b81c
5h5ong <Call5h5ong@gmail.com>
2019-10-13 18:30:32 +0900

    create HomeCard component

---
+++ src/Components/HomeCard.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import styled from '../typed-components';
+
+type HomeCardProps = {
+  id: number;
+  filename: string;
+  url: string;
+};
+
+const HomeCard = ({ id, filename, url }: HomeCardProps) => {};
+
+export default HomeCard;
