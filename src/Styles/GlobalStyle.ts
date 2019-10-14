dcd84d5195d1a27ac5f8cb7c7123603f8b21e6f3
5h5ong <Call5h5ong@gmail.com>
2019-10-15 00:10:21 +0900

    add new global font

--- src/Styles/GlobalStyle.ts
+++ src/Styles/GlobalStyle.ts
@@ -2,6 +2,8 @@ import { createGlobalStyle } from '../typed-components';
 
 export default createGlobalStyle`
   body {
+    @import url('https://fonts.googleapis.com/css?family=Roboto&display=swap');
+    font-family: 'Roboto', sans-serif;
     background-color: ${props => props.theme.bgColor}
   }
 `;
