24bd3ed95544ea9e2c3b89058f1812926e49fc01
5h5ong <Call5h5ong@gmail.com>
2019-10-14 23:30:59 +0900

    create and add header

--- src/App.tsx
+++ src/App.tsx
@@ -4,10 +4,12 @@ import { HashRouter as Router } from 'react-router-dom';
 import { theme } from './Styles/Style';
 import GlobalStyle from './Styles/GlobalStyle';
 import Routes from './Components/Routes';
+import Header from './Components/Header';
 
 const Container = styled.div`
   display: flex;
   justify-content: center;
+  flex-direction: column;
 `;
 
 const App = () => {
@@ -16,6 +18,7 @@ const App = () => {
       <>
         <GlobalStyle />
         <Container>
+          <Header />
           <Router>
             <Routes isLoggedIn={true} />
           </Router>
---
+++ src/Components/Header.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import styled from '../typed-components';
+
+const Container = styled.header``;
+
+const Header = () => {
+  return (
+    <Container>
+      <div>HEADER</div>
+    </Container>
+  );
+};
+
+export default Header;
