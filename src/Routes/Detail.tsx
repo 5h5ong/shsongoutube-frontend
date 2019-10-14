378dfeda1e07b19c7aa9d78fbb76cb690383210a
5h5ong <Call5h5ong@gmail.com>
2019-10-14 09:33:21 +0900

    change flex direction

--- src/Routes/Detail.tsx
+++ src/Routes/Detail.tsx
@@ -1,14 +1,20 @@
 import React from 'react';
 import { withRouter } from 'react-router-dom';
+import styled from '../typed-components';
 import VideoPlayer from '../Components/VideoPlayer';
+
+const Container = styled.div`
+  justify-content: center;
+  flex-direction: column;
+`;
 
 const Detail = ({ match, location }: any) => {
   const { filename, videoId } = match.params;
   return (
-    <>
+    <Container>
       <div>{filename}</div>
       <VideoPlayer id={parseInt(videoId)} />
-    </>
+    </Container>
   );
 };
 
