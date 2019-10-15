b6890bcb587b86052cad7167a34cb2d86dc376b5
5h5ong <Call5h5ong@gmail.com>
2019-10-15 13:13:54 +0900

    perfect align!

--- src/Components/Header.tsx
+++ src/Components/Header.tsx
@@ -2,15 +2,17 @@ import React from 'react';
 import styled from '../typed-components';
 
 const Container = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
   background-color: white;
-  padding-top: 20px;
   text-align: center;
   width: 100%;
   height: 70px;
   margin-bottom: 50px;
   box-shadow: 0 5px 10px 0 rgba(0, 64, 128, 0.05);
 `;
-const HeaderText = styled.div`
+const HeaderText = styled.span`
   background-color: inherit;
   font-size: 35px;
   font-weight: 1000;
