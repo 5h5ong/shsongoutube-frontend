2467d4e3551cc7c607fa75cdb02b68fbf5de9c96
5h5ong <Call5h5ong@gmail.com>
2019-10-15 23:01:58 +0900

    remove needless styles

--- src/Components/VideoPlayer.tsx
+++ src/Components/VideoPlayer.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { useQuery } from 'react-apollo-hooks';
 import { gql } from 'apollo-boost';
 import ReactPlayer from 'react-player';
-import styled from 'styled-components';
+import styled from '../typed-components';
 
 // set type inside props
 type Props = {
@@ -10,12 +10,7 @@ type Props = {
 };
 
 // styled-components
-const Container = styled.div`
-  background-color: white;
-  border-radius: 16px;
-  justify-content: center;
-  box-shadow: ${props => props.theme.boxShadow};
-`;
+const Container = styled.div``;
 
 const GET_FILE = gql`
   query getFile($id: Int!) {
