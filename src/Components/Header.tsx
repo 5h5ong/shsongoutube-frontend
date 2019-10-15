34d13ea23ecee78ae9cd737cc8efd9331edeef14
5h5ong <Call5h5ong@gmail.com>
2019-10-15 12:55:07 +0900

    add beautiful shadow

--- src/Components/Header.tsx
+++ src/Components/Header.tsx
@@ -1,12 +1,18 @@
 import React from 'react';
 import styled from '../typed-components';
 
-const Container = styled.header`
+const Container = styled.div`
+  background-color: white;
+  padding-top: 20px;
   text-align: center;
-  padding: 20px;
+  width: 100%;
+  height: 70px;
+  margin-bottom: 50px;
+  box-shadow: 0 5px 10px 0 rgba(0, 64, 128, 0.05);
 `;
 const HeaderText = styled.div`
-  font-size: 50px;
+  background-color: inherit;
+  font-size: 35px;
   font-weight: 1000;
 `;
 const Header = () => {
