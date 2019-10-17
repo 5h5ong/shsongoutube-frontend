bbf6ac6aff89f01093c921433b94055f104c4071
5h5ong <Call5h5ong@gmail.com>
2019-10-17 23:58:32 +0900

    preparing process to enable the apollo

--- src/Routes/Auth/AuthContainer.tsx
+++ src/Routes/Auth/AuthContainer.tsx
@@ -1,11 +1,22 @@
 import React, { useState } from 'react';
+import { useMutation } from 'react-apollo-hooks';
+import { CHECK_EMAIL } from './AuthQueries';
 import AuthPresenter from './AuthPresenter';
 import useInput from '../../Hooks/useInput';
 
 const AuthContainer = () => {
   const [loginAction, setLoginAction] = useState('login');
   const emailInput = useInput('');
-  return <AuthPresenter loginAction={loginAction} emailInput={emailInput} />;
+
+  const onSubmit = async (event: React.FormEvent<HTMLFormElement>) => {};
+
+  return (
+    <AuthPresenter
+      loginAction={loginAction}
+      emailInput={emailInput}
+      onSubmit={onSubmit}
+    />
+  );
 };
 
 export default AuthContainer;
