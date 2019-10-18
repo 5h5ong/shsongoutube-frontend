e4fb359354c9cb7f9a376c171313c4c731482884
5h5ong <Call5h5ong@gmail.com>
2019-10-18 19:25:41 +0900

    confirm 완료

--- src/Routes/Auth/AuthContainer.tsx
+++ src/Routes/Auth/AuthContainer.tsx
@@ -1,9 +1,8 @@
 import React, { useState } from 'react';
 import { useMutation } from 'react-apollo-hooks';
-import { CHECK_EMAIL } from './AuthQueries';
+import { CHECK_EMAIL, LOGIN, LOCAL_LOGIN } from './AuthQueries';
 import AuthPresenter from './AuthPresenter';
 import useInput from '../../Hooks/useInput';
-import { AnyTxtRecord } from 'dns';
 
 interface checkEmailInventory {
   checkEmail: boolean;
@@ -11,10 +10,15 @@ interface checkEmailInventory {
 
 const AuthContainer = () => {
   const [loginAction, setLoginAction] = useState('login');
-  const emailInput = useInput('call5h5ong@gmail.com');
+  const emailInput = useInput('');
+  const secretInput = useInput('');
   const [checkEmailMutation] = useMutation(CHECK_EMAIL, {
     variables: { email: emailInput.value }
   });
+  const [loginMutation] = useMutation(LOGIN, {
+    variables: { email: emailInput.value, secretKey: secretInput.value }
+  });
+  const [localLoginMutation] = useMutation(LOCAL_LOGIN);
 
   const onSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault();
@@ -24,9 +28,19 @@ const AuthContainer = () => {
       }: any = await checkEmailMutation();
       // check email
       if (checkEmail) {
-        setLoginAction('signup');
+        setLoginAction('confirm');
       } else {
         console.error('존재하지 않는 이메일입니다!');
+      }
+    } else if (loginAction === 'confirm') {
+      try {
+        const {
+          data: { login }
+        }: any = await loginMutation();
+        // frontend에 token 전달
+        await localLoginMutation({ variables: { token: login } });
+      } catch (e) {
+        console.error(e);
       }
     }
   };
@@ -36,6 +50,7 @@ const AuthContainer = () => {
       loginAction={loginAction}
       emailInput={emailInput}
       onSubmit={onSubmit}
+      secretInput={secretInput}
     />
   );
 };
--- src/apolloClient.ts
+++ src/apolloClient.ts
@@ -1,5 +1,21 @@
 import ApolloClient from 'apollo-boost';
 
+const defaults = {
+  isLoggedIn: Boolean(localStorage.getItem('token')) || false
+};
+const resolvers = {
+  Mutation: {
+    localLogin: (_: any, { token }: any) => {
+      console.log(token);
+      localStorage.setItem('token', token);
+    }
+  }
+};
+
 export default new ApolloClient({
-  uri: 'http://localhost:4000'
+  uri: 'http://localhost:4000',
+  clientState: {
+    defaults,
+    resolvers
+  }
 });
