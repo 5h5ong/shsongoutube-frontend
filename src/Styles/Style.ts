44c1e70a34ae44052ca826a6bf94a0ce626fbe5d
5h5ong <Call5h5ong@gmail.com>
2019-10-18 23:19:54 +0900

    기본 button style

--- src/Styles/Style.ts
+++ src/Styles/Style.ts
@@ -1,10 +1,10 @@
 const BOX_SHADOW =
   '0 12px 19px 2px rgba(34,0,51,.04),0 6px 22px 4px rgba(7,48,114,.12),0 6px 10px -4px rgba(14,13,26,.12);';
 const BORDER_RADIUS = '16px';
+const BTN_COLOR = '#0060df';
 
 export const theme = {
   bgColor: '#fbfbfe',
-  btnColor: '#0060df',
   headerColor: '#bbded6',
   linkColor: 'black',
   whiteBox: `background-color: white;
@@ -20,6 +20,20 @@ export const theme = {
                border-radius: 8px;
                font-size: 16px;
                font-weight: 400;
+  `,
+  basicButton: `
+    width: 100%;
+    height: 48px;
+    border: none;
+    border-radius: 8px;
+    padding: 10px;
+    background-color: ${BTN_COLOR} ;
+    color: white;
+    font-size: 16px;
+    font-weight: 400;
+    &:hover {
+      background-color: #003eaa;
+    }
   `,
   minHeight: '800px',
   minWidth: '512px'
