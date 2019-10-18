3385a5b044674e53fbeb4d0780d458cc75846cfb
5h5ong <Call5h5ong@gmail.com>
2019-10-18 19:36:51 +0900

    update defaults cache

--- src/apolloClient.ts
+++ src/apolloClient.ts
@@ -5,9 +5,14 @@ const defaults = {
 };
 const resolvers = {
   Mutation: {
-    localLogin: (_: any, { token }: any) => {
+    localLogin: (_: any, { token }: any, { chche }: any) => {
       console.log(token);
       localStorage.setItem('token', token);
+      // update defaults vaule
+      chche.writeData({
+        isLoggedIn: true
+      });
+      return;
     }
   }
 };
