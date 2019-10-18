4dc8f726f64fe836dd94bac0c55ef197e2d86024
5h5ong <Call5h5ong@gmail.com>
2019-10-18 12:21:04 +0900

    login 구현 완료

--- src/Routes/Auth/AuthContainer.tsx
+++ src/Routes/Auth/AuthContainer.tsx
@@ -3,12 +3,33 @@ import { useMutation } from 'react-apollo-hooks';
 import { CHECK_EMAIL } from './AuthQueries';
 import AuthPresenter from './AuthPresenter';
 import useInput from '../../Hooks/useInput';
+import { AnyTxtRecord } from 'dns';
+
+interface checkEmailInventory {
+  checkEmail: boolean;
+}
 
 const AuthContainer = () => {
   const [loginAction, setLoginAction] = useState('login');
-  const emailInput = useInput('');
+  const emailInput = useInput('call5h5ong@gmail.com');
+  const [checkEmailMutation] = useMutation(CHECK_EMAIL, {
+    variables: { email: emailInput.value }
+  });
 
-  const onSubmit = async (event: React.FormEvent<HTMLFormElement>) => {};
+  const onSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    if (loginAction === 'login') {
+      const {
+        data: { checkEmail }
+      }: any = await checkEmailMutation();
+      // check email
+      if (checkEmail) {
+        setLoginAction('signup');
+      } else {
+        console.error('존재하지 않는 이메일입니다!');
+      }
+    }
+  };
 
   return (
     <AuthPresenter
