ca116d0038fdb6c434a6f0c0bb97f0bd155a9ae6
5h5ong <Call5h5ong@gmail.com>
2019-10-19 12:09:03 +0900

    create like button

--- src/Routes/Detail.tsx
+++ src/Routes/Detail.tsx
@@ -3,7 +3,8 @@ import { withRouter } from 'react-router-dom';
 import styled from '../typed-components';
 import VideoPlayer from '../Components/VideoPlayer';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faThumbsUp } from '@fortawesome/free-solid-svg-icons';
+import { faThumbsUp as fasThumbsUp } from '@fortawesome/free-solid-svg-icons';
+import { faThumbsUp as farThumbsUp } from '@fortawesome/free-regular-svg-icons';
 
 const Container = styled.div`
   ${props => props.theme.whiteBox}
@@ -15,21 +16,34 @@ const Container = styled.div`
 `;
 const ContentContainer = styled.div`
   display: flex;
+  width: 100%;
+  margin-top: 30px;
+  flex-direction: row;
+  justify-content: flex-start;
 `;
 const Filename = styled.span`
   font-size: 20px;
   margin-bottom: 40px;
 `;
+const Icon = styled.div``;
 
 const Detail = ({ match }: any) => {
   const [isLiked, setIsLiked] = useState(false);
   const { filename, videoId } = match.params;
+  // functions
+  const onClick = () => {
+    setIsLiked(i => !i);
+  };
+
   return (
     <Container>
       <Filename>{filename}</Filename>
       <VideoPlayer id={parseInt(videoId)} />
       <ContentContainer>
-        <FontAwesomeIcon icon={faThumbsUp} />
+        <Icon onClick={onClick}>
+          {isLiked && <FontAwesomeIcon icon={fasThumbsUp} size='2x' />}
+          {!isLiked && <FontAwesomeIcon icon={farThumbsUp} size='2x' />}
+        </Icon>
       </ContentContainer>
     </Container>
   );
