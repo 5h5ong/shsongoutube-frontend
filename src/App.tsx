63022646cce21dfefd4898be03cfaf1e4fe23909
5h5ong <Call5h5ong@gmail.com>
2019-10-21 19:47:32 +0900

    replace isloggedin gql

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,17 @@
 import React from 'react';
 import styled, { ThemeProvider } from './typed-components';
 import { HashRouter as Router } from 'react-router-dom';
-import { gql } from 'apollo-boost';
 import { useQuery } from 'react-apollo-hooks';
 import { theme } from './Styles/Style';
 import GlobalStyle from './Styles/GlobalStyle';
 import Routes from './Components/Routes';
+import { IS_LOGGED_IN } from './globalQueries';
 
 const Container = styled.div`
   display: flex;
   justify-content: center;
   align-items: center;
   flex-direction: column;
-`;
-const IS_LOGGED_IN = gql`
-  {
-    isLoggedIn @client
-  }
 `;
 
 const App = () => {
