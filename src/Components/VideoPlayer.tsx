e9e628aff66d757d7f6cafa1bec947b59e8b4138
5h5ong <Call5h5ong@gmail.com>
2019-10-21 17:05:36 +0900

    필요없는 query 제거를 위한 구조 변경

--- src/Components/VideoPlayer.tsx
+++ src/Components/VideoPlayer.tsx
@@ -1,33 +1,19 @@
 import React from 'react';
-import { useQuery } from 'react-apollo-hooks';
-import { gql } from 'apollo-boost';
 import ReactPlayer from 'react-player';
 import styled from '../typed-components';
 
 // set type inside props
-type Props = {
-  id: number;
-};
+interface VideoPlayerProps {
+  url: string;
+}
 
 const Container = styled.div``;
 
-const GET_FILE = gql`
-  query getFile($id: Int!) {
-    getFile(id: $id) {
-      url
-    }
-  }
-`;
-
-const VideoPlayer = ({ id }: Props) => {
-  const { data, loading } = useQuery(GET_FILE, { variables: { id } });
+const VideoPlayer = (props: VideoPlayerProps) => {
   return (
     <Container>
       <div>
-        {loading && <div> loading....</div>}
-        {!loading && data && data.getFile && data.getFile.url && (
-          <ReactPlayer url={data.getFile.url} playing controls />
-        )}
+        <ReactPlayer url={props.url} playing controls />
       </div>
     </Container>
   );
